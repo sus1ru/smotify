6f3a9d995a61cddcffef159260c2b5c466713699
sus1ru <sus1ru.s4n@gmail.com>
2025-07-17 14:46:31 +0545

    feat: initialized the post app

--- smotify/settings.py
+++ smotify/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user',
+    'post',
     'django_extensions',
     'rest_framework',
     'rest_framework.authtoken',
