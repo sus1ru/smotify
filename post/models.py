42d670e4773b8cd8e85016cbd04df3b3e4e1ab3a
sus1ru <sus1ru.s4n@gmail.com>
2025-07-17 15:13:33 +0545

    feat: added model for Post and Comment

--- post/models.py
+++ post/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from treebeard.mp_tree import MP_Node
+
+
+class BaseModel(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="%(class)ss",
+        blank=True,
+        null=True,
+    )
+    upvotes = models.PositiveIntegerField(default=0, blank=True, null=True)
+    downvotes = models.PositiveIntegerField(default=0, blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    date_updated = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(BaseModel):
+    title = models.CharField(max_length=256, null=True, blank=True)
+
+
+class Comment(BaseModel, MP_Node):
+    post = models.ForeignKey(
+        "Post", on_delete=models.CASCADE, related_name="comments", blank=True, null=True
+    )
+
+    node_order_by = ["-date_created"]
